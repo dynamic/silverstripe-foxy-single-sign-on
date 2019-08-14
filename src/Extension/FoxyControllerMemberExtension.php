2af94e2c6b6b741a5ee403e58a109f4cff41de12
Jason Irish <jsirish@users.noreply.github.com>
2019-08-14 09:11:11 -0500

    FEATURE MemberFactory (#9)
    fixes #5

---
+++ src/Extension/FoxyControllerMemberExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dynamic\Foxy\SingleSignOn\Extension;
+
+use Dynamic\Foxy\Parser\Foxy\Transaction;
+use Dynamic\Foxy\SingleSignOn\Factory\MemberFactory;
+use SilverStripe\Core\Extension;
+
+class FoxyControllerMemberExtension extends Extension
+{
+    /**
+     * @param Transaction $transaction
+     * @throws \SilverStripe\ORM\ValidationException
+     */
+    public function doAdditionalParse(Transaction $transaction)
+    {
+        MemberFactory::create($transaction)->getOrder();
+    }
+}
---
+++ src/Factory/MemberFactory.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Dynamic\Foxy\SingleSignOn\Factory;
+
+use Dynamic\Foxy\Orders\Factory\FoxyFactory;
+use Dynamic\Foxy\Parser\Foxy\Transaction;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Extensible;
+use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\Security;
+use SilverStripe\View\ArrayData;
+
+class MemberFactory
+{
+    use Configurable;
+    use Extensible;
+    use Injectable;
+
+    /**
+     * @var Transaction
+     */
+    private $transaction;
+
+    /**
+     * @var
+     */
+    private $member;
+
+    /**
+     * OrderDetailFactory constructor.
+     * @param Transaction|null $transaction
+     */
+    public function __construct(Transaction $transaction = null)
+    {
+        if ($transaction instanceof Transaction && $transaction !== null) {
+            $this->setTransaction($transaction);
+        }
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return $this
+     */
+    public function setTransaction(Transaction $transaction)
+    {
+        $this->transaction = $transaction;
+
+        return $this;
+    }
+
+    /**
+     * @return Transaction
+     */
+    protected function getTransaction()
+    {
+        return $this->transaction;
+    }
+
+    /**
+     * @return Member
+     */
+    public function getMember()
+    {
+        if (!$this->member instanceof Member) {
+            $this->setMember();
+        }
+
+        return $this->member;
+    }
+
+    protected function setMember()
+    {
+        /** @var ArrayData $transaction */
+        $transaction = $this->getTransaction()->getParsedTransactionData()->getField('transaction');
+
+        // if not a guest transaction in Foxy
+        if ($transaction->getField('customer_email')
+            && $transaction->getField('is_anonymous') == 0) {
+
+            /** @var $encryption - disable password encryption to prevent double encryption */
+            $encryption = Config::inst()->get(Security::class, 'password_encryption_algorithm');
+            Config::modify()->set(Security::class, 'password_encryption_algorithm', 'none');
+
+            if (!$customer = Member::get()->filter('Email', $transaction->getField('customer_email'))->first()) {
+                $customer = Member::create();
+            }
+
+            foreach ($this->config()->get('member_mapping') as $foxy => $ssFoxy) {
+                if ($transaction->hasField($foxy)) {
+                    $order->{$ssFoxy} = $transaction->getField($foxy);
+                }
+            }
+
+            /** manuall set encryption type to sha1 */
+            $customer->PasswordEncryption = 'sha1';
+
+            /** flag to prevent push to Foxy on write */
+            $customer->FromDataFeed = true;
+
+            $customer->write();
+
+            /** re-enable password encryption */
+            Config::modify()->set(Security::class, 'password_encryption_algorithm', $encryption);
+        }
+    }
+}
